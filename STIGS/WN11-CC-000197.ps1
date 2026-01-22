25046c589ee43e10ffc2b7bbf39867d14607971c
JacobKingVA <jacobkingva@gmail.com>
2026-01-22 15:12:08 -0500

    Create WN11-CC-000197.ps1

---
+++ STIGS/WN11-CC-000197.ps1
@@ -0,0 +1,45 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that Microsoft consumer experiences are turned off.
+
+.NOTES
+    Author          : Jacob King
+    LinkedIn        : linkedin.com/in/jkingva/
+    GitHub          : github.com/jacobkingva
+    Date Created    : 2026-01-22
+    Last Modified   : 2026-01-22
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-AU-000197
+
+.TESTED ON
+    Date(s) Tested  : 2026-01-22
+    Tested By       : Jacob King
+    Systems Tested  : Windows 11 Pro
+    PowerShell Ver. : Windows PowerShell ISE
+
+.USAGE
+    Apply using PowerShell ISE as an administrator. Script success will be reflected in the registry as well as in successful compliance scans.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN11-AU-000197).ps1 
+#>
+
+# Requires administrative privileges
+
+$regPath  = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent"
+$valueName = "DisableWindowsConsumerFeatures"
+$valueData = 1
+
+# Ensure the registry key exists
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+# Create or update the DWORD value
+New-ItemProperty `
+    -Path $regPath `
+    -Name $valueName `
+    -PropertyType DWord `
+    -Value $valueData `
+    -Force | Out-Null
