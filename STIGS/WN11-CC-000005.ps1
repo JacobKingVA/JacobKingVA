6a705ae48e4900d716464fd2bb3c9516284ff562
JacobKingVA <jacobkingva@gmail.com>
2026-01-26 11:38:18 -0500

    Create WN11-CC-000005.ps1

---
+++ STIGS/WN11-CC-000005.ps1
@@ -0,0 +1,60 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that camera access from the lock screen must be disabled.
+.NOTES
+    Author          : Jacob King
+    LinkedIn        : linkedin.com/in/jkingva/
+    GitHub          : github.com/jacobkingva
+    Date Created    : 2026-01-26
+    Last Modified   : 2026-01-26
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000005
+
+.TESTED ON
+    Date(s) Tested  : 2026-01-26
+    Tested By       : Jacob King
+    Systems Tested  : Windows 11 Pro
+    PowerShell Ver. : Windows PowerShell ISE
+
+.USAGE
+    Apply using PowerShell ISE as an administrator. Script success will be reflected in the registry as well as in successful compliance scans.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN11-CC-000005).ps1 
+#>
+
+# Define registry path and value
+$RegPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization"
+$Name    = "NoLockScreenCamera"
+$Value   = 1
+
+# Create the key if it doesn't exist
+if (-not (Test-Path $RegPath)) {
+    New-Item -Path $RegPath -Force | Out-Null
+}
+
+# Set the DWORD value
+New-ItemProperty `
+    -Path $RegPath `
+    -Name $Name `
+    -Value $Value `
+    -PropertyType DWord `
+    -Force | Out-Null
+# Define registry path and value
+$RegPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization"
+$Name    = "NoLockScreenCamera"
+$Value   = 1
+
+# Create the key if it doesn't exist
+if (-not (Test-Path $RegPath)) {
+    New-Item -Path $RegPath -Force | Out-Null
+}
+
+# Set the DWORD value
+New-ItemProperty `
+    -Path $RegPath `
+    -Name $Name `
+    -Value $Value `
+    -PropertyType DWord `
+    -Force | Out-Null
